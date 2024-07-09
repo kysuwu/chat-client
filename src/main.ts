7361113a9ddaef237b05f1576bc975a458bb52be
no <no>
2024-07-09 16:20:26 +0200

    chore: pkg, ghactions

--- src/main.ts
+++ src/main.ts
@@ -5,54 +5,60 @@ import { getMessageRepo } from "./factories/getmessagerepo.js";
 import { getUserRepo } from "./factories/getuserrepo.js";
 import { ChatService } from "./services/chat-service.js";
 import { UI } from "./ui.js";
-import { resolve } from "node:path"
-import os from 'os';
+import { resolve } from "node:path";
+import os from "os";
 import Yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-const HOME = os.homedir();
-const COOKIE_FILE = HOME + '/anonchat.json';
+async function main() {
+  const HOME = os.homedir();
+  const COOKIE_FILE = HOME + "/anonchat.json";
 
-const argv = await Yargs(hideBin(process.argv)).option("profile", {
-  alias: "p",
-  type: "string",
-  default: COOKIE_FILE
-}).argv;
+  const argv = await Yargs(hideBin(process.argv)).option("profile", {
+    alias: "p",
+    type: "string",
+    default: COOKIE_FILE,
+  }).argv;
 
-const profile = resolve(argv.profile);
+  const profile = resolve(argv.profile);
 
-let destroyUI: (() => void) | null = null;
+  let destroyUI: (() => void) | null = null;
 
-try {
-  const ui = new UI((messageText) => {
-    chatService.sendMessage(messageText);
-  });
-  destroyUI = () => {
-    ui.destroy();
-  }
+  try {
+    const ui = new UI((messageText) => {
+      chatService.sendMessage(messageText);
+    });
+    destroyUI = () => {
+      ui.destroy();
+    };
 
-  const cookieRepo = getCookieRepo(profile);
-  const messageRepo = getMessageRepo();
-  const userRepo = getUserRepo();
-  const chatService = new ChatService(userRepo, messageRepo, cookieRepo);
-  const actionStream = chatService.run();
-  for await (const action of actionStream) {
-    switch (action.type) {
-      case "NEW_MESSAGE": {
-        ui.addMessage(action.payload);
-        break;
-      }
-      case "AUTH": {
-        ui.setUser(action.payload);
-        break
-      }
-      case "AUTH_ERROR": {
-        throw new Error("FAILED TO AUTHENTICATE; REMOVE " + profile + " AND RESTART");
+    const cookieRepo = getCookieRepo(profile);
+    const messageRepo = getMessageRepo();
+    const userRepo = getUserRepo();
+    const chatService = new ChatService(userRepo, messageRepo, cookieRepo);
+    const actionStream = chatService.run();
+    for await (const action of actionStream) {
+      switch (action.type) {
+        case "NEW_MESSAGE": {
+          ui.addMessage(action.payload);
+          break;
+        }
+        case "AUTH": {
+          ui.setUser(action.payload);
+          break;
+        }
+        case "AUTH_ERROR": {
+          throw new Error(
+            "FAILED TO AUTHENTICATE; REMOVE " + profile + " AND RESTART"
+          );
+        }
       }
     }
+  } catch (e) {
+    destroyUI?.();
+    console.error("FAILED TO INITIALIZE", e);
+    process.exit(1);
   }
-} catch (e) {
-  destroyUI?.();
-  console.error("FAILED TO INITIALIZE", e);
-  process.exit(1);
 }
+
+main();
